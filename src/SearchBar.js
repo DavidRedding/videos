a36d3d4e1d64e311bb9caa15df6c339def94c582
David Redding <redding.david35@gmail.com>
2021-12-19 13:44:10 -0600

    search state and css

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,13 +1,24 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  state = {
+    term: "",
+  };
+
   render() {
     return (
-      <div>
-        <form>
-          <label></label>
-          <input type="text" placeholder='Try "charlie bit me"'></input>
-          <button>Search</button>
+      <div className="search-bar ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Video Search </label>
+            <input
+              type="text"
+              placeholder='Try "charlie bit my finger"'
+              value={this.state.term}
+              onChange={(e) => this.setState({ term: e.target.value })}
+            ></input>
+            <button>Search</button>
+          </div>
         </form>
       </div>
     );
