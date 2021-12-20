478f54c7f0c497cb74af987cbbf66a7a513492b8
David Redding <redding.david35@gmail.com>
2021-12-20 05:49:48 -0600

    same

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,11 @@
 const KEY = "AIzaSyCYJB164cjYY6772widfAuHg5P9YCHqlWw";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    type: "video", // stops the unique "key" prop error msg
+    maxResults: 5,
+    key: KEY,
+  },
+});
