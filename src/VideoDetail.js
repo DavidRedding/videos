15819e0397f82946cc2222214c9293e4204aa207
David Redding <redding.david35@gmail.com>
2021-12-25 12:16:26 -0600

    built out video desciption

--- src/VideoDetail.js
+++ src/VideoDetail.js
@@ -3,7 +3,12 @@ import React from "react";
 const VideoDetail = (props) => {
   if (props.video == null) return <div>Loading...</div>;
 
-  return <div> {props.video.snippet.title}</div>;
+  return (
+    <div className="ui segment">
+      <h4 className="ui header"> {props.video.snippet.title}</h4>
+      <p>{props.video.snippet.description}</p>
+    </div>
+  );
 };
 
 export default VideoDetail;
