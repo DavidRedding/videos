e15e6deadf3f216c573dcad4111fb366722cd57b
David Redding <redding.david35@gmail.com>
2022-01-07 08:20:10 -0600

    renamed onTermSubmit, wrote return statement

--- src/hooks/useVideos.js
+++ src/hooks/useVideos.js
@@ -9,12 +9,15 @@ I will return a list of videos & a way to search for videos(via function/method)
 const useVideos = (defaultSearchTerm) => {
   const [videos, setVideos] = useState([]);
 
-  useEffect(() => onTermSubmit(defaultSearchTerm), []);
+  useEffect(() => search(defaultSearchTerm), []);
 
-  const onTermSubmit = async (term) => {
+  const search = async (term) => {
     const response = await youtube.get("/search", { params: { q: term } });
     setVideos(response.data.items);
   };
+
+  // returning our promised search function & list of videos
+  return { videos, search };
 };
 
 export default useVideos;
