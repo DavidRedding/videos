30f17d3f86cce5126ce13eadf997fac7f9cf4f79
David Redding <redding.david35@gmail.com>
2022-01-07 08:15:13 -0600

    made "defaultSearchTerm" a customizable arg

--- src/hooks/useVideos.js
+++ src/hooks/useVideos.js
@@ -6,10 +6,10 @@ If you give me a default search term,
 I will return a list of videos & a way to search for videos(via function/method)
 */
 
-const useVideos = () => {
+const useVideos = (defaultSearchTerm) => {
   const [videos, setVideos] = useState([]);
 
-  useEffect(() => onTermSubmit(`Charlie bit my finger`), []);
+  useEffect(() => onTermSubmit(defaultSearchTerm), []);
 
   const onTermSubmit = async (term) => {
     const response = await youtube.get("/search", { params: { q: term } });
