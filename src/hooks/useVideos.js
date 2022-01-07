f37089bb0653eb9a58827a5824259d35fd208f77
David Redding <redding.david35@gmail.com>
2022-01-07 08:06:05 -0600

    custom "useVideos"  Hook init

---
+++ src/hooks/useVideos.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+import youtube from "../apis/youtube";
+
+/*
+If you give me a default search term, 
+I will return a list of videos & a way to search for videos(via function/method)
+*/
+
+const useVideos = () => {};
+
+export default useVideos;
