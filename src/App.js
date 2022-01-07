004d2bfad8f3690714e0b88b2b33f6214157cb87
David Redding <redding.david35@gmail.com>
2022-01-07 07:32:44 -0600

    useEffect and full code refactor

--- src/App.js
+++ src/App.js
@@ -7,42 +7,30 @@ import VideoDetail from "./VideoDetail";
 const App = () => {
   const [videos, setVideos] = useState([]);
   const [selectedVideo, setSelectedVideo] = useState(null);
-};
 
-class App extends React.Component {
-  componentDidMount() {
-    this.onTermSubmit(`Charlie bit my finger`);
-  }
+  useEffect(() => onTermSubmit(`Charlie bit my finger`), []);
 
-  onTermSubmit = async (term) => {
+  const onTermSubmit = async (term) => {
     const response = await youtube.get("/search", { params: { q: term } });
-    this.setState({
-      videos: response.data.items,
-      selectedVideo: response.data.items[0],
-    });
+    setVideos(response.data.items);
+    setSelectedVideo(response.data.items[0]);
   };
 
-  onVideoSelect = (video) => this.setState({ selectedVideo: video });
-
-  render() {
-    return (
-      <div className="ui container">
-        <SearchBar onTermSubmit={this.onTermSubmit} />
-        <div className="ui grid">
-          <div className="row">
-            <div className="eleven wide column">
-              <VideoDetail video={this.state.selectedVideo} />
-            </div>
-            <div className="five wide column">
-              <VideoList
-                videos={this.state.videos}
-                onVideoSelect={this.onVideoSelect}
-              />
-            </div>
+  return (
+    <div className="ui container">
+      <SearchBar onTermSubmit={onTermSubmit} />
+      <div className="ui grid">
+        <div className="row">
+          <div className="eleven wide column">
+            <VideoDetail video={selectedVideo} />
+          </div>
+          <div className="five wide column">
+            <VideoList videos={videos} onVideoSelect={setSelectedVideo} />
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
 export default App;
