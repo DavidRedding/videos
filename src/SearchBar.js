83827928f735081aa7d57d6e5d5533b94f95341b
David Redding <redding.david35@gmail.com>
2022-01-07 07:04:10 -0600

    refactored 'props' & 'this' statements

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,33 +1,29 @@
 import React, { useState } from "react";
 
-const SearchBar = () => {
+const SearchBar = ({ onTermSubmit }) => {
   const [term, setTerm] = useState("");
-};
 
-class SearchBar extends React.Component {
-  onFormSubmit = (e) => {
+  const onFormSubmit = (e) => {
     e.preventDefault();
-    // test to see if 'props' was neccesary
-    this.props.onTermSubmit(this.state.term);
+    onTermSubmit(term);
   };
 
-  render() {
-    return (
-      <div className="search-bar ui segment">
-        <form onSubmit={this.onFormSubmit} className="ui form">
-          <div className="field">
-            <label>Video Search </label>
-            <input
-              type="text"
-              placeholder={`"charlie bit my finger"`}
-              value={this.state.term}
-              onChange={(e) => this.setState({ term: e.target.value })}
-            ></input>
-            <button>Search</button>
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="search-bar ui segment">
+      <form onSubmit={onFormSubmit} className="ui form">
+        <div className="field">
+          <label>Video Search </label>
+          <input
+            type="text"
+            placeholder={`"charlie bit my finger"`}
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+          ></input>
+          <button>Search</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
 export default SearchBar;
